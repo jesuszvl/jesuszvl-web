3cce2483f010c126d25da232bd79f9c9cb7fce1f
jesuszvl <jesus.zavala@gametime.co>
2023-02-11 01:16:12 -0800

    add GA tracking

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 
+import ReactGA from "react-ga4";
+
 import Profile from "./components/Profile/Profile";
 import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
 import Footer from "./components/Footer/Footer";
+
+ReactGA.initialize("G-K32RW6EE92");
+ReactGA.send({ hitType: "pageview", page: "/" });
 
 const App = () => {
   return (
