70e91ac9e001ef83a7461a98d86b57cac247f153
jesuszvl <jesus.zavala@gametime.co>
2023-06-01 03:38:53 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import About from "../src/components/About/About";
 import Experience from "../src/components/Experience/Experience";
 import Footer from "../src/components/Footer/Footer";
 import Navbar from "../src/components/Navbar/Navbar";
+import Head from "next/head";
 
 ReactGA.initialize("G-K32RW6EE92");
 ReactGA.send({ hitType: "pageview", page: "/" });
@@ -12,6 +13,9 @@ ReactGA.send({ hitType: "pageview", page: "/" });
 export default function Index() {
   return (
     <div className="container">
+      <Head>
+        <title>JESUSZVL | Home</title>
+      </Head>
       <Navbar />
       <Profile />
       <About />
