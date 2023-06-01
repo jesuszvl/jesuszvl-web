57b157840c59e5cf68c9d34d207e27e4e5bfebb9
jesuszvl <jesus.zavala@gametime.co>
2023-06-01 02:56:46 -0700

    add navbar and improve profile

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import styles from "../src/styles/Index.module.scss";
-
 import ReactGA from "react-ga4";
 
 import Profile from "../src/components/Profile/Profile";
 import About from "../src/components/About/About";
 import Experience from "../src/components/Experience/Experience";
 import Footer from "../src/components/Footer/Footer";
+import Navbar from "../src/components/Navbar/Navbar";
 
 ReactGA.initialize("G-K32RW6EE92");
 ReactGA.send({ hitType: "pageview", page: "/" });
@@ -13,6 +12,7 @@ ReactGA.send({ hitType: "pageview", page: "/" });
 export default function Index() {
   return (
     <div className="container">
+      <Navbar />
       <Profile />
       <About />
       <Experience />
