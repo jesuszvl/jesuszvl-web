9483030e744cc92aa1c5f4a39f644bdc13640991
jesuszvl <jesus.zavala@gametime.co>
2023-12-29 05:23:55 -0800

    remove sass

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,24 +1,60 @@
 import React from 'react'
 import styles from './ProjectCard.module.scss'
-import { motion } from 'framer-motion'
+import { motion, MotionConfig } from 'framer-motion'
+import classNames from 'classnames'
+import makahco from '../../images/portfolio/makahco.png'
 
 interface Props {
   title: string
   description: string
   image: any
   url: string
+  color: string
+  isTextLight?: boolean
 }
 
-const ProjectCard: React.FC<Props> = ({ title, description, image, url }) => {
+const ProjectCard: React.FC<Props> = ({
+  title,
+  description,
+  image,
+  url,
+  color,
+  isTextLight
+}) => {
   return (
-    <motion.div whileHover={{ scale: 1.05 }} className={styles.project}>
-      <a href={url} target='_blank' rel='noopener noreferrer'>
-        <section className={styles.content}>
-          <h1 className={styles.title}>{title}</h1>
-          <p className={styles.description}>{description}</p>
-        </section>
-      </a>
-    </motion.div>
+    <MotionConfig transition={{ duration: 0.15 }}>
+      <motion.div
+        whileHover={{ scale: 1.05 }}
+        className={styles.project}
+        style={{ background: color }}
+      >
+        <a href={url} target='_blank' rel='noopener noreferrer'>
+          <section className={styles.content}>
+            <h1
+              className={classNames([
+                styles.title,
+                {
+                  [styles.isTextLight]: isTextLight
+                }
+              ])}
+            >
+              {title}
+            </h1>
+            <p
+              className={classNames([
+                styles.description,
+                {
+                  [styles.isTextLight]: isTextLight
+                }
+              ])}
+            >
+              {description}
+            </p>
+            <img src={makahco.src} alt={title} className={styles.image} />
+          </section>
+        </a>
+      </motion.div>
+    </MotionConfig>
   )
 }
 
